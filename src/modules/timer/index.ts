8aa5f15499d5a1a320419a6dc76d5506ed83e543
mei23 <m@m544.net>
2018-11-08 02:11:32 +0900

    タイマーは同じチャネルで返す

--- src/modules/timer/index.ts
+++ src/modules/timer/index.ts
@@ -42,9 +42,7 @@ export default class TimerModule implements IModule {
 
 			setTimeout(() => {
 				const name = msg.friend.name;
-				this.ai.sendMessage(msg.userId, {
-					text: serifs.timer.notify(str, name)
-				});
+				msg.reply(serifs.timer.notify(str, name));
 			}, time);
 
 			return true;
