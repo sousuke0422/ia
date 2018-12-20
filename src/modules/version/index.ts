c12030141f8d5a1ff496a1576df68b4901688343
mei23 <m@m544.net>
2018-12-20 17:44:51 +0900

    Version link mention

--- src/modules/version/index.ts
+++ src/modules/version/index.ts
@@ -39,12 +39,8 @@ export default class VersionModule implements IModule {
 				const clientChanged = this.latest.client !== fetched.client;
 
 				if (serverChanged || clientChanged) {
-					const newServer = fetched.server.match(/^\d+\.\d+\.\d+$/)
-						? `[${fetched.server}](https://github.com/syuilo/misskey/releases/tag/${fetched.server})`
-						: fetched.server;
-
 					let v = '';
-					v += (serverChanged ? '**' : '') + `Server: ${this.latest.server} → ${newServer}\n` + (serverChanged ? '**' : '');
+					v += (serverChanged ? '**' : '') + `Server: ${this.latest.server} → ${this.mfmVersion(fetched.server)}\n` + (serverChanged ? '**' : '');
 					v += (clientChanged ? '**' : '') + `Clinet: ${this.latest.client} → ${fetched.client}\n` + (clientChanged ? '**' : '');
 
 					console.log(`Version changed: ${v}`);
@@ -67,7 +63,7 @@ export default class VersionModule implements IModule {
 		if (query == null) return false;
 
 		this.ai.api('meta').then(meta => {
-			msg.reply(`${meta.version}\n(Client: ${meta.clientVersion || '???'})みたいです。`)
+			msg.reply(`${this.mfmVersion(meta.version)} (Client: ${meta.clientVersion || '???'})みたいです。`)
 		}).catch(() => {
 			msg.reply(`取得失敗しました`)
 		});
@@ -85,6 +81,13 @@ export default class VersionModule implements IModule {
 				client: meta.clientVersion
 			};
 		});
+	}
+
+	private mfmVersion = (v): string => {
+		if (v == null) return v;
+		return v.match(/^\d+\.\d+\.\d+$/)
+		? `[${v}](https://github.com/syuilo/misskey/releases/tag/${v})`
+		: v.server;
 	}
 
 	private wait = (ms: number): Promise<void> => {
