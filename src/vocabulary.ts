8ed43f0e9355bc8c4fecd2217dce458b4cf88e6f
mei23 <m@m544.net>
2020-10-31 22:19:20 +0900

    学習ワードが使える時はワードをシンプルに

--- src/vocabulary.ts
+++ src/vocabulary.ts
@@ -269,12 +269,17 @@ export function genItem(seedOrRng?: (() => number) | string | number, getKeyword
 		: Math.random;
 
 	let item = '';
-	if (Math.floor(rng() * 5) !== 0) item += itemPrefixes[Math.floor(rng() * itemPrefixes.length)];
-	item += ((getKeyword ? getKeyword(rng) : null) || items[Math.floor(rng() * items.length)]);
-	if (Math.floor(rng() * 3) === 0) {
-		item += and[Math.floor(rng() * and.length)];
+	if (getKeyword) {
 		if (Math.floor(rng() * 5) !== 0) item += itemPrefixes[Math.floor(rng() * itemPrefixes.length)];
 		item += ((getKeyword ? getKeyword(rng) : null) || items[Math.floor(rng() * items.length)]);
+	} else {
+		if (Math.floor(rng() * 5) !== 0) item += itemPrefixes[Math.floor(rng() * itemPrefixes.length)];
+		item += (items[Math.floor(rng() * items.length)]);
+		if (Math.floor(rng() * 3) === 0) {
+			item += and[Math.floor(rng() * and.length)];
+			if (Math.floor(rng() * 5) !== 0) item += itemPrefixes[Math.floor(rng() * itemPrefixes.length)];
+			item += (items[Math.floor(rng() * items.length)]);
+		}
 	}
 	return item;
 }
